cc7ccad615e54baef511e6252d2da027bbedaf56
Miguel Bernal <migueljos8711@yahoo.com>
2021-02-10 15:12:43 +0100

    avancement

---
+++ app.py
@@ -0,0 +1,41 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, precision_recall_curve, auc, roc_auc_score, precision_score, roc_curve, recall_score, classification_report
+from sklearn.metrics import make_scorer
+import pandas as pd
+
+def cost(truth, preds):
+    precision = precision_score(truth, preds)
+    recall = recall_score(truth, preds)
+    # square beta
+    beta2 = 2.5**2
+    f_mesure = ((1+beta2)*precision*recall)/(beta2*(precision+recall))
+    return f_mesure
+
+custom_scorer = make_scorer(cost, greater_is_better=False)
+
+app = Flask(
+    __name__,
+    static_url_path='/static', static_folder='static',
+    template_folder='templates')
+
+model = pickle.load(open('/Users/migueljosebernalmora/P7_Bernal_Miguel/model_lr.sav', 'rb'))
+data = pd.read_csv('data_model_red.csv', index_col='index')
+data_stat =pd.read_csv('/Users/migueljosebernalmora/P7_Bernal_Miguel/data_dashboard.csv', index_col='SK_ID_CURR')
+
+@app.route('/predict/<int:client_id>')
+def predict(client_id):
+    x =data.loc[client_id].values.reshape(1, -1)
+    pred = model.predict(x)[0]
+    proba_pay = model.predict_proba(x)[0][0]*100
+    proba_pay = f'{proba_pay:.4}'
+    print(proba_pay)
+
+    return render_template('dashboard.html', client_id=client_id, pred=pred, proba_pay=proba_pay )
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
